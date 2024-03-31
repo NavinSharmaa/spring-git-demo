96d50a90db6d6882dc375fc4389d8765e3c82a29
NavinSharmaa <snavin266@gmail.com>
2024-03-31 12:28:32 +0530

    logger added

--- src/main/java/com/demo/SpringGitDemoApplication.java
+++ src/main/java/com/demo/SpringGitDemoApplication.java
@@ -1,12 +1,17 @@
 package com.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringGitDemoApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringGitDemoApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("class loaded");
 		SpringApplication.run(SpringGitDemoApplication.class, args);
 	}
 
